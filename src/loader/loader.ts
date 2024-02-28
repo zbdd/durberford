acff8f95905ed7c7a9515a8a733d0093ae440359
Zac Durber <zdurber@gmail.com>
2024-02-28 20:54:03 +0800

    correctly adding gameobject to parent gameobject

--- src/loader/loader.ts
+++ src/loader/loader.ts
@@ -1,4 +1,5 @@
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
+import type { GameObjectProps } from '../view';
 import { GameObject } from '../view';
 import type { AnimationAction, Group, Object3D, Object3DEventMap } from 'three';
 import { AnimationMixer, TextureLoader } from 'three';
@@ -21,35 +22,57 @@ export class Loader extends FBXLoader {
         this.mixers = mixers;
     }
 
-    public async loadGameAssets(
-        assets: GameAssetProps[],
-    ): Promise<{ gameObject: GameObject; attachTo?: string; onAttached?: (object: Object3D) => void }[]> {
-        const gameObjects: { gameObject: GameObject; attachTo?: string; onAttached?: (object: Object3D) => void }[] =
-            [];
+    public async loadGameAssets(assetsGroup: GameAssetProps[][]): Promise<GameObject[]> {
+        const gameObjects: GameObject[] = [];
+        for (const group of assetsGroup) {
+            const objectsAndActions: {
+                modelObject: Object3D;
+                actions: AnimationAction[];
+                attachTo?: string;
+                onAttached?: (object: Object3D) => void;
+            }[] = [];
 
-        for (const asset of assets) {
-            const loadedObject = await this.loadAsset(asset);
-            gameObjects.push({
-                gameObject: new GameObject({
-                    gameObjectProps: [
-                        {
-                            name: asset.name,
-                            object: loadedObject.modelObject,
-                            actions: loadedObject.actions,
-                        },
-                    ],
-                }),
-                attachTo: asset.attachTo,
-                onAttached: asset.onAttached,
+            for (const asset of group) {
+                const loadedObject = await this.loadAsset(asset);
+                objectsAndActions.push(loadedObject);
+            }
+
+            const gameObjectProps: GameObjectProps[] = [];
+            objectsAndActions.forEach((pair, index) => {
+                gameObjectProps.push({
+                    name: pair.modelObject.name,
+                    object: pair.modelObject,
+                    actions: pair.actions,
+                    attachTo:
+                        index === 0
+                            ? undefined
+                            : pair.attachTo
+                              ? this.findObjectByName(pair.attachTo, gameObjectProps[0].object)
+                              : gameObjectProps[0].object,
+                });
+                pair.onAttached?.(pair.modelObject);
             });
+            gameObjects.push(new GameObject({ gameObjectProps }));
         }
 
         return gameObjects;
     }
 
+    private findObjectByName(name: string, objectToSearch: Object3D): Object3D | undefined {
+        let object: Object3D | undefined;
+
+        objectToSearch.traverse((child) => {
+            if (child.name.trim() === name) object = child;
+        });
+
+        return object;
+    }
+
     private async loadAsset(asset: GameAssetProps): Promise<{
         modelObject: Group<Object3DEventMap>;
         actions: AnimationAction[];
+        attachTo?: string;
+        onAttached?: (object: Object3D) => void;
     }> {
         const modelObject = await this.loadAsync(asset.modelPath);
         if (!modelObject) throw Error(`Failed to load asset: ${asset.name}`);
@@ -79,6 +102,8 @@ export class Loader extends FBXLoader {
         return {
             modelObject,
             actions,
+            attachTo: asset.attachTo,
+            onAttached: asset.onAttached,
         };
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import type { Object3D } from 'three';
 import * as THREE from 'three';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import type { GameObject } from './view';
 import { View } from './view';
 import { Loader } from './loader';
 
@@ -20,50 +19,57 @@ const loader = new Loader({ mixers });
 
 const loadObjects = async (): Promise<void> => {
     const gameObjects = await loader.loadGameAssets([
-        {
-            modelPath: 'assets/units/SkeletonWarrior_2HandAxe/Models/skeleton_body_mesh.fbx',
-            texturePath: 'assets/units/SkeletonWarrior_2HandAxe/Textures/skeleton_body_D.png',
-            name: 'skeleton-body',
-            animationPath: 'assets/units/SkeletonWarrior_2HandAxe/Animations/SkeletonWarrior_2h_ultimate.fbx',
-        },
-        {
-            modelPath: 'assets/units/SkeletonWarrior_2HandAxe/Models/skeleton_set1_mesh.fbx',
-            texturePath: 'assets/units/SkeletonWarrior_2HandAxe/Textures/skeleton_set1_D.png',
-            name: 'skeleton-clothing',
-            animationPath: 'assets/units/SkeletonWarrior_2HandAxe/Animations/SkeletonWarrior_2h_ultimate.fbx',
-        },
-        {
-            modelPath: 'assets/units/SkeletonWarrior_2HandAxe/Models/skeleton_2h_axe.fbx',
-            texturePath: 'assets/units/SkeletonWarrior_2HandAxe/Textures/skeleton_2HAxe_D.png',
-            name: 'skeleton-axe',
-            attachTo: 'weapon_end',
-            onAttached: (object: Object3D) => {
-                object.rotateY(90);
-                object.rotateZ(90);
+        [
+            {
+                modelPath: 'assets/units/SkeletonWarrior_2HandAxe/Models/skeleton_body_mesh.fbx',
+                texturePath: 'assets/units/SkeletonWarrior_2HandAxe/Textures/skeleton_body_D.png',
+                name: 'skeleton-body',
+                animationPath: 'assets/units/SkeletonWarrior_2HandAxe/Animations/SkeletonWarrior_2h_ultimate.fbx',
             },
-        },
+            {
+                modelPath: 'assets/units/SkeletonWarrior_2HandAxe/Models/skeleton_set1_mesh.fbx',
+                texturePath: 'assets/units/SkeletonWarrior_2HandAxe/Textures/skeleton_set1_D.png',
+                name: 'skeleton-clothing',
+                animationPath: 'assets/units/SkeletonWarrior_2HandAxe/Animations/SkeletonWarrior_2h_ultimate.fbx',
+            },
+            {
+                modelPath: 'assets/units/SkeletonWarrior_2HandAxe/Models/skeleton_2h_axe.fbx',
+                texturePath: 'assets/units/SkeletonWarrior_2HandAxe/Textures/skeleton_2HAxe_D.png',
+                name: 'skeleton-axe',
+                attachTo: 'weapon_end',
+                onAttached: (object: Object3D) => {
+                    object.rotateY(90);
+                    object.rotateZ(90);
+                },
+            },
+        ],
     ]);
-    const gameGroup: GameObject[] = [];
+    gameObjects.forEach((gameObject) => view.add(gameObject));
+    // const gameGroup: GameObject[] = [];
 
-    gameObjects.forEach((loadedObject) => {
-        const { gameObject, attachTo, onAttached } = loadedObject;
-        const characterName = gameObject.name.split('-')[0];
-        const foundObj = gameGroup.find((obj: GameObject) => obj.name.split('-')[0] === characterName);
-
-        if (foundObj) {
-            if (attachTo) {
-                foundObj.traverse((child) => {
-                    if (child.name.trim() === attachTo) {
-                        child.add(gameObject);
-                        onAttached?.(gameObject);
-                    }
-                });
-            } else foundObj.add(gameObject);
-        } else {
-            gameGroup.push(gameObject);
-            view.add(gameObject);
-        }
-    });
+    // gameObjects.forEach((loadedObject) => {
+    //     const { gameObject, attachTo, onAttached } = loadedObject;
+    //     const characterName = gameObject.name.split('-')[0];
+    //     const foundObj = gameGroup.find((obj: GameObject) => obj.name.split('-')[0] === characterName);
+    //
+    //     if (foundObj) {
+    //         if (attachTo) {
+    //             foundObj.traverse((child) => {
+    //                 if (child.name.trim() === attachTo) {
+    //                     child.add(gameObject);
+    //                     onAttached?.(gameObject);
+    //                 }
+    //             });
+    //         } else {
+    //             foundObj.add(gameObject);
+    //         }
+    //     } else {
+    //         gameGroup.push(gameObject);
+    //         view.add(gameObject);
+    //     }
+    // });
+    // gameObjects[0].gameObject.getActions()[0]?.play();
+    // gameObjects[1].gameObject.getActions()[0]?.play();
 };
 
 void loadObjects();
